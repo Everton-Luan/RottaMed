6df6c11ce857d02d99288cada0131620ad6a573a
Everton <everton.luan@ufrpe.br>
2026-06-10 20:11:15 -0300

    :sparkles: feat: implementa controller e view do dev para cadastrar unidades

---
+++ Controllers/Dev_controller.py
@@ -0,0 +1,35 @@
+from Views.Unidade_view import *
+
+ARQUIVO =  "database/unidades.txt"
+
+def salvar_unidade(unidade):
+    with open (ARQUIVO, "a", encoding="utf-8") as arquivo:
+
+        linha = (
+            f"{unidade['nome']};"
+            f"{unidade['tipo']};"
+            f"{unidade['fluxo']};"
+            f"{unidade['especializações']};"
+            f"{unidade['cpf_gestor']};"
+            f"{unidade['senha_gestor']}\n"
+        )
+
+        arquivo.write(linha)
+
+def iniciar_cadastro_unidade():
+    # 1. Chama a View para pegar os dados
+    nome, tipo, fluxo, espec, cpf, senha = add_dados_unidade()
+    
+    # 2. Empacota
+    unidade_dict = {
+        "nome": nome,
+        "tipo": tipo,
+        "fluxo": fluxo,
+        "especializações": espec,
+        "cpf_gestor": cpf,
+        "senha_gestor": senha
+    }
+    
+    # 3. Salva
+    salvar_unidade(unidade_dict)
+  
