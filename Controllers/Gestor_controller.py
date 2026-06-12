358899451ceea6eb93e7f22b338613b291283915
MiAlbuquerque <mirellaalbuquerquesousa@gmail.com>
2026-06-12 15:57:11 -0300

    atualizacoes

--- Controllers/Gestor_controller.py
+++ Controllers/Gestor_controller.py
@@ -1,41 +1,66 @@
-# Importe os arquivos necessários dependendo de onde colocar esta função
-from Models import Unidade_model
-from Views import Unidade_view
+import Models.Unidade_model as modelU 
+import Views.Unidade_view as viewU
+import Views.Gestor_view as viewG
 
-def iniciar_atualizacao_gestor():
-    print("\n===== ATUALIZAR UNIDADE (GESTOR) =====")
+def iniciar_tela_gestor():
+    #Pede as credenciais do Gestor
+    cpf_digitado, senha_digitada = viewG.pedir_credenciais()
     
-    # 1. Pede o nome para buscar (reutilizando a função que você já tem)
-    unidade_procurada = Unidade_view.pedir_nome_busca().upper()
+    #Carrega todo o banco de dados na memória
+    lista_unidades = modelU.ler_dados_txt()
     
-    # 2. Puxa a lista completa e procura
-    lista_unidades = Unidade_model.ler_dados_txt() # Use a sua função de leitura aqui
-    indice_encontrado = -1
+    #Variáveis para guardar o login do usuário
+    unidade_logada = None
+    indice_logado = -1
     
+    #Busca o CPF e a Senha a partir dos dados existentes no dicionário
     for i, u in enumerate(lista_unidades):
-        if u["nome"].upper() == unidade_procurada:
-            indice_encontrado = i 
-            break 
+        if u["cpf_gestor"] == cpf_digitado and u["senha_gestor"] == senha_digitada:
+            unidade_logada = u
+            indice_logado = i
+            break #Após encontrar, para o for.
             
-    # 3. Se não achou, encerra
-    if indice_encontrado == -1:
-        print("\n❌ Unidade não encontrada.")
-        return 
+    #Caso não encontre, não permite a entrada na área do gestor
+    if unidade_logada is None:
+        print("\n❌ Login falhou: CPF ou senha incorretos ou não cadastrados.")
+        return #O usuário retorna ao menu 
         
-    # 4. Separa a unidade encontrada em uma variável para ficar mais fácil de ler
-    unidade = lista_unidades[indice_encontrado]
+    print(f"\n✅ Acesso liberado! Bem-vindo(a) gestor(a) da {unidade_logada['nome'].title()}.")
     
-    # 5. Manda a unidade para a View perguntar os novos dados
-    novo_fluxo, nova_espec = Unidade_view.gestor_atualiza_dados(unidade)
+    #O laço infinito de navegação do gestor
+    while True:
+        opcao = viewG.menu_gestor(unidade_logada["nome"])
+        
+        match opcao:
+            case "1":
+            
+                viewU.exibir_unidade_unica(unidade_logada)
+                
+            case "2":
+                
+                unidade_logada = iniciar_atualizacao_gestor(unidade_logada, indice_logado, lista_unidades)         
 
-    # Nós NÃO mexemos no nome, tipo, CPF ou senha. Só substituímos estas duas chaves!
+            case "3":
+                print("\n👋 Fazendo logout... Retornando ao Menu Principal.")
+                break #Quebra o while
+
+
+
+def iniciar_atualizacao_gestor(unidade, indice, lista_unidades):
+    
+    #Aparece na view para o gestor atualizar os dados
+    novo_fluxo, nova_espec = viewU.gestor_atualiza_dados(unidade)
+
+    #é trocado essas duas chaves lá no dicionário
     unidade["fluxo"] = novo_fluxo
     unidade["especializações"] = nova_espec
     
-    # Devolvemos a unidade alterada para a lista
-    lista_unidades[indice_encontrado] = unidade
+    #A lista principal é atualizada
+    lista_unidades[indice] = unidade
     
-    # 6. Manda o Model salvar a lista inteira por cima do arquivo velho
-    Unidade_model.modificar_unidades(lista_unidades)
+    #O Model salva a lista nova
+    modelU.modificar_unidades(lista_unidades)
     
     print("\n✅ Fluxo e Especializações atualizados com sucesso!")
+    
+    return unidade
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Views.Menu_principal_view as viewM
 import Controllers.cidadao_controller as controllerC
+import Controllers.Gestor_controller as controllerG
 
 def iniciar_sistema():
     while True:
-        # Puxa a sua função que exibe a tela e devolve 1, 2, 3 ou 4
+        #Puxa a sua função que exibe a tela e devolve 1, 2, 3 ou 4
         opcao = viewM.exibir_menu_principal()
 
         match opcao:
@@ -12,17 +13,9 @@ def iniciar_sistema():
                 controllerC.iniciar_tela_cidadao()
                 
             case "2":
-                print("\n" + "=" * 45)
-                print(" 🚧 TELA DO GESTOR (Em desenvolvimento) 🚧".center(45))
-                print("=" * 45)
-                if viewM.confirmacao_volta():
-                    continue
-                else:
-                    print("Saindo...")
-                    break
-                # No futuro, você vai apenas desmarcar a linha abaixo:
-                # controllerG.iniciar_tela_gestor()
-                
+            
+                controllerG.iniciar_tela_gestor()
+            
             case "3":
                 print("\n" + "=" * 45)
                 print(" 🚧 TELA DESENVOLVEDOR (Em desenvolvimento) 🚧".center(45))
@@ -41,7 +34,7 @@ def iniciar_sistema():
                 print("=" * 45)
                 break # Quebra o while principal, desligando o programa
 
-# Essa condição garante que o sistema só rode se este arquivo for executado diretamente
+#Essa condição garante que o sistema só rode se este arquivo for executado diretamente
 if __name__ == "__main__":
     iniciar_sistema()
 
